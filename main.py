f873995eaedc040b656ca224a3c01a685f864ded
Adwait <adwait.kul.2018@gmail.com>
2022-12-01 10:17:02 -0800

    Added game over on win functionality

--- main.py
+++ main.py
@@ -8,12 +8,16 @@ display = []
 for letter in chosen_word:
   display.append("_")
 
-user_input = input("Guess a letter:")
-guess = user_input.lower()
-
-for position in range(len(chosen_word)):
-  letter = chosen_word[position]
-  if (letter == guess):
-    display[position] = letter
-
-print(display)
+while True:
+  if "_" not in display:
+    print("You win.")
+    break
+  else:  
+    user_input = input("Guess a letter:")
+    guess = user_input.lower()
+    
+    for position in range(len(chosen_word)):
+      letter = chosen_word[position]
+      if (letter == guess):
+        display[position] = letter
+    print(display)
