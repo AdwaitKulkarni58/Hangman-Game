5a393376f2657a65dab90bd6e348742ffb6419b9
Adwait <adwait.kul.2018@gmail.com>
2022-12-01 12:19:52 -0800

    Completed game logic

--- main.py
+++ main.py
@@ -61,6 +61,8 @@ word_list = ["aardvark", "baboon", "camel"]
 
 chosen_word = word_list[random.randint(0, len(word_list) - 1)]
 
+lives = 6
+
 display = []
 for letter in chosen_word:
   display.append("_")
@@ -69,12 +71,20 @@ while True:
   if "_" not in display:
     print("You win.")
     break
-  else:  
-    user_input = input("Guess a letter:")
-    guess = user_input.lower()
+  user_input = input("Guess a letter:")
+  guess = user_input.lower()
     
-    for position in range(len(chosen_word)):
-      letter = chosen_word[position]
-      if (letter == guess):
-        display[position] = letter
-    print(display)
+  for position in range(len(chosen_word)):
+    letter = chosen_word[position]
+    if (letter == guess):
+      display[position] = letter
+  
+  print("".join(display))
+  
+  if guess not in display:
+    lives = lives - 1
+    if (lives == 0):
+      print(stages[0])
+      print("You lose")
+      break
+  print(stages[lives])
