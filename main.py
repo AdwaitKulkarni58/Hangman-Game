8e04062ec45640114b1b16af77740f08ccc10362
Adwait <adwait.kul.2018@gmail.com>
2022-11-30 17:51:00 -0800

    Added blanks for letters

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ word_list = ["aardvark", "baboon", "camel"]
 
 chosen_word = word_list[random.randint(0, len(word_list) - 1)]
 
+display = []
+for letter in chosen_word:
+  display.append("_")
+
 user_input = input("Guess a letter:")
 guess = user_input.lower()
 
-for letter in chosen_word:
+for position in range(len(chosen_word)):
+  letter = chosen_word[position]
   if (letter == guess):
-    print("right")
-  else:
-    print("wrong")
+    display[position] = letter
+
+print(display)
